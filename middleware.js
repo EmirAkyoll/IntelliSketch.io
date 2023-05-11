24b3b8748b19a34292e45a85bbd79241c641101c
Emir Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-05-11 15:59:02 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,6 @@
 import { NextResponse } from "next/server";
-import { applyRateLimit } from "./utils/rate-limiter";
 
-export async function middleware(request) {
-  const response = NextResponse.next();
 
-  try {
-    await applyRateLimit(request, response);
-  } catch (error) {
-    console.log(error);
-    return new NextResponse("Too Many Request", { status: 429 });
-  }
+export async function middleware(request) {}
 
-  return response;
-}
-
-export const config = {
-  matcher: "/api/(.*)",
-};
+export const config = {matcher: "/api/(.*)"};
